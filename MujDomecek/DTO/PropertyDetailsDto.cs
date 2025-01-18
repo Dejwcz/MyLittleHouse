2029fb13a0cfb04b5a1114b4dc3b3655031c5ed2
David Smětala <x213@centrum.cz>
2025-01-18 20:57:50 +0100

    AdminViewForButtonsInPURImpoved

--- MujDomecek/DTO/PropertyDetailsDto.cs
+++ MujDomecek/DTO/PropertyDetailsDto.cs
@@ -3,7 +3,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } 
         public DateTime? UpdatedAt { get; set; }
 
         // Navigation property
